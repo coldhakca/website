4ebdd757d3863bb8d59a3d239607bd6ea9f446e5
Colin Childs <colin@torproject.org>
2015-06-02 00:58:22 -0500

    Fixed coldkernel view and adjusted server.go for nginx proxying

--- server.go
+++ server.go
@@ -6,7 +6,10 @@ import (
 )
 
 func renderTemplate(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/"):]
+        title := r.URL.Path[len("/"):]
+    if title == "" {
+        title = "coldkernel"
+    }
     t, err := template.ParseFiles("views/" + title + ".html")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -18,30 +21,11 @@ func renderTemplate(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func addDefaultHeaders(fn http.HandlerFunc) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        if origin := r.Header.Get("Origin"); origin != "" {
-            w.Header().Set("Access-Control-Allow-Origin", origin)
-        }
-        //w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubdomains")
-        w.Header().Set("X-Frame-Options", "DENY")
-        w.Header().Set("Content-Security-Policy", "'self'")
-        fn(w, r)
-    }
-}
-
-func redir(w http.ResponseWriter, req *http.Request) {
-	http.Redirect(w, req, "https://coldhak.ca" + req.RequestURI, http.StatusMovedPermanently)
-}
-
 func main() {
     http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
     http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
     http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
-    http.HandleFunc("/coldkernel", addDefaultHeaders(renderTemplate))
-    http.ListenAndServe(":8080", http.HandlerFunc(redir))
-    err := http.ListenAndServeTLS(":8443", "/etc/nginx/conf/ssl-unified.crt", "/etc/nginx/conf/ssl.key", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+    http.HandleFunc("/coldkernel", renderTemplate)
+    http.HandleFunc("/", renderTemplate)
+    http.ListenAndServe(":3000", nil)
 }
